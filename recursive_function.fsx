73232dc79a83962194e9be8f923a599a73575241
Sanya <sanya2371@gmail.com>
2016-10-10 13:38:44 +0300

    recursive functions

---
+++ recursive_function.fsx
@@ -0,0 +1,11 @@
+﻿let rec length = function
+    | [] -> 0
+    | x::xs -> 1 + length xs
+    
+     
+let rec factorial x = 
+    if x < 2 then
+        1
+    else
+        x * factorial (x - 1)
+         
