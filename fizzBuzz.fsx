c1bf78ab8d1cca0bc552cb5e0423457fff64ed07
Sanya <sanya2371@gmail.com>
2016-10-12 16:50:25 +0300

    FuzzBIzz challenge

---
+++ fizzBuzz.fsx
@@ -0,0 +1,11 @@
+﻿let (|MultOf3|_|) i = if i % 3 = 0 then Some MultOf3 else None
+let (|MultOf5|_|) i = if i % 5 = 0 then Some MultOf5 else None
+
+let fuzzBizz n =
+    match n with
+    |MultOf3 & MultOf5 -> printf "FuzzBizz, "
+    |MultOf3 -> printf "Fuzz, "
+    |MultOf5 -> printf "Bizz, "
+    |_ -> printf "%i, " n
+
+[1..99] |> List.iter fuzzBizz
